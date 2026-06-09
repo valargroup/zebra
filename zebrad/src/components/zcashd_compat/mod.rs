8b19bae2a15a57acd879b04b36750e3a18081961
Roman Akhtariev <ackhtariev@gmail.com>
2026-06-09 14:44:13 -0300

    refactor: min hardware check (#30)
    * refactor: min hardware check
    
    * allow bypassing non-Linux hosts
    
    * address Cgroup v1 limit never consulted
    
    * add comments and bug fix

--- zebrad/src/components/zcashd_compat/mod.rs
+++ zebrad/src/components/zcashd_compat/mod.rs
@@ -3,6 +3,7 @@
 mod config;
 mod managed;
 mod manifest;
+mod preflight;
 mod supervisor;
 
 pub use config::{Config, ZcashdBinarySource as ConfigZcashdBinarySource};
@@ -14,4 +15,5 @@ pub use manifest::{
     ZcashdReleaseArtifact, ZcashdReleaseManifest, EMBEDDED_MANIFEST_SCHEMA_VERSION,
     EMBEDDED_ZCASHD_RELEASE_MANIFEST,
 };
+pub use preflight::run_preflight;
 pub use supervisor::{is_command_resolvable, run as run_supervisor, SupervisorConfig};
