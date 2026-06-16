382ee3c1f428170de3f2613edd93daa3ccb0cfb1
Adam Tucker <adam@osmosis.team>
2026-06-15 19:23:09 -0600

    feat(rpc): expose Ironwood RPC and network data

--- zebrad/tests/acceptance.rs
+++ zebrad/tests/acceptance.rs
@@ -783,14 +783,13 @@ fn last_config_is_stored() -> Result<()> {
         fs::read_to_string(generated_config_path).expect("Should have been able to read the file");
 
     // We need to replace the cache dir path as stored configs has a dummy `cache_dir` string there.
+    let default_cache_dir = zebra_state::Config::default()
+        .cache_dir
+        .to_str()
+        .expect("a valid cache dir")
+        .to_string();
     let processed_generated_content = generated_content
-        .replace(
-            zebra_state::Config::default()
-                .cache_dir
-                .to_str()
-                .expect("a valid cache dir"),
-            "cache_dir",
-        )
+        .replace(&default_cache_dir, "cache_dir")
         .trim()
         .to_string();
 
@@ -847,7 +846,7 @@ fn last_config_is_stored() -> Result<()> {
          Or run: \n\
          cargo build --bin zebrad && \n\
          zebrad generate | \n\
-         sed 's/cache_dir = \".*\"/cache_dir = \"cache_dir\"/' > \n\
+         sed 's|{default_cache_dir}|cache_dir|g' > \n\
          zebrad/tests/common/configs/<next-release-tag>.toml",
     ))
 }
@@ -3229,7 +3228,7 @@ async fn trusted_chain_sync_handles_forks_correctly() -> Result<()> {
             NetworkUpgrade::Nu5
             | NetworkUpgrade::Nu6
             | NetworkUpgrade::Nu6_1
-            | NetworkUpgrade::Nu7 => {
+            | NetworkUpgrade::Nu6_3 => {
                 ChainHistoryBlockTxAuthCommitmentHash::from_commitments(&hist_root, &auth_root)
                     .bytes_in_serialized_order()
             }
@@ -3599,8 +3598,6 @@ async fn nu6_funding_streams_and_coinbase_balance() -> Result<()> {
         Height(block_template.height()),
         &miner_params,
         Amount::zero(),
-        #[cfg(all(zcash_unstable = "nu7", feature = "tx_v6"))]
-        None,
     )
     .expect("coinbase transaction should be valid under the given parameters");
 
@@ -3666,8 +3663,6 @@ async fn nu6_funding_streams_and_coinbase_balance() -> Result<()> {
         Height(block_template.height()),
         &miner_params,
         Amount::zero(),
-        #[cfg(all(zcash_unstable = "nu7", feature = "tx_v6"))]
-        None,
     )
     .expect("coinbase transaction should be valid under the given parameters");
 
@@ -3721,169 +3716,6 @@ async fn nu6_funding_streams_and_coinbase_balance() -> Result<()> {
     // Check that the original block template can be submitted successfully
     let proposal_block = proposal_block_from_template(&valid_original_block_template, None, &net)?;
 
-    let submit_block_response = rpc
-        .submit_block(HexData(proposal_block.zcash_serialize_to_vec()?), None)
-        .await?;
-
-    assert_eq!(
-        submit_block_response,
-        SubmitBlockResponse::Accepted,
-        "valid block should be accepted"
-    );
-
-    Ok(())
-}
-
-/// Test successful block template submission as a block proposal.
-///
-/// This test can be run locally with:
-/// `RUSTFLAGS='--cfg zcash_unstable="nu7"' cargo test --package zebrad --test acceptance --features tx_v6 -- nu7_nsm_transactions --exact --show-output`
-#[tokio::test(flavor = "multi_thread")]
-#[cfg(all(zcash_unstable = "nu7", feature = "tx_v6"))]
-async fn nu7_nsm_transactions() -> Result<()> {
-    use zebra_chain::{
-        chain_sync_status::MockSyncStatus,
-        parameters::testnet::{self, ConfiguredActivationHeights, ConfiguredFundingStreams},
-        serialization::ZcashSerialize,
-        work::difficulty::U256,
-    };
-    use zebra_network::address_book_peers::MockAddressBookPeers;
-    use zebra_node_services::mempool;
-    use zebra_rpc::client::HexData;
-    use zebra_test::mock_service::MockService;
-    let _init_guard = zebra_test::init();
-
-    tracing::info!("running nu7_nsm_transactions test");
-
-    let base_network_params = testnet::Parameters::build()
-        // Regtest genesis hash
-        .with_genesis_hash("029f11d80ef9765602235e1bc9727e3eb6ba20839319f761fee920d63401e327")
-        .unwrap()
-        .with_checkpoints(false)
-        .unwrap()
-        .with_target_difficulty_limit(U256::from_big_endian(&[0x0f; 32]))
-        .unwrap()
-        .with_disable_pow(true)
-        .with_slow_start_interval(Height::MIN)
-        .with_lockbox_disbursements(vec![])
-        .with_activation_heights(ConfiguredActivationHeights {
-            nu7: Some(1),
-            ..Default::default()
-        });
-
-    let network = base_network_params
-        .clone()
-        .unwrap()
-        .with_funding_streams(vec![ConfiguredFundingStreams {
-            // Start checking funding streams from block height 1
-            height_range: Some(Height(1)..Height(100)),
-            // Use default post-NU6 recipients
-            recipients: None,
-        }])
-        .to_network()
-        .unwrap();
-
-    tracing::info!("built configured Testnet, starting state service and block verifier");
-
-    let default_test_config = default_test_config(&network);
-    let mining_config = default_test_config.mining;
-
-    let (state, read_state, latest_chain_tip, _chain_tip_change) =
-        zebra_state::init_test_services(&network).await;
-
-    let (
-        block_verifier_router,
-        _transaction_verifier,
-        _parameter_download_task_handle,
-        _max_checkpoint_height,
-    ) = zebra_consensus::router::init_test(
-        zebra_consensus::Config::default(),
-        &network,
-        state.clone(),
-    )
-    .await;
-
-    tracing::info!("started state service and block verifier, committing Regtest genesis block");
-
-    let genesis_hash = block_verifier_router
-        .clone()
-        .oneshot(zebra_consensus::Request::Commit(regtest_genesis_block()))
-        .await
-        .expect("should validate Regtest genesis block");
-
-    let mut mempool = MockService::build()
-        .with_max_request_delay(Duration::from_secs(5))
-        .for_unit_tests();
-    let mut mock_sync_status = MockSyncStatus::default();
-    mock_sync_status.set_is_close_to_tip(true);
-
-    let submitblock_channel = SubmitBlockChannel::new();
-
-    let (_tx, rx) = tokio::sync::watch::channel(None);
-
-    let (rpc, _) = RpcImpl::new(
-        network.clone(),
-        mining_config,
-        false,
-        "0.0.1",
-        "Zebra tests",
-        mempool.clone(),
-        state.clone(),
-        read_state.clone(),
-        block_verifier_router,
-        mock_sync_status,
-        latest_chain_tip,
-        MockAddressBookPeers::default(),
-        rx,
-        Some(submitblock_channel.sender()),
-    );
-
-    let make_mock_mempool_request_handler = || async move {
-        mempool
-            .expect_request(mempool::Request::FullTransactions)
-            .await
-            .respond(mempool::Response::FullTransactions {
-                transactions: vec![],
-                transaction_dependencies: Default::default(),
-                // tip hash needs to match chain info for long poll requests
-                last_seen_tip_hash: genesis_hash,
-            });
-    };
-
-    let block_template_fut = rpc.get_block_template(None);
-    let mock_mempool_request_handler = make_mock_mempool_request_handler.clone()();
-    let (block_template, _) = tokio::join!(block_template_fut, mock_mempool_request_handler);
-    let GetBlockTemplateResponse::TemplateMode(block_template) =
-        block_template.expect("unexpected error in getblocktemplate RPC call")
-    else {
-        panic!("this getblocktemplate call without parameters should return the `TemplateMode` variant of the response")
-    };
-
-    let proposal_block = proposal_block_from_template(&block_template, None, &network)?;
-    let hex_proposal_block = HexData(proposal_block.zcash_serialize_to_vec()?);
-
-    // Check that the block template is a valid block proposal
-    let GetBlockTemplateResponse::ProposalMode(block_proposal_result) = rpc
-        .get_block_template(Some(GetBlockTemplateParameters::new(
-            GetBlockTemplateRequestMode::Proposal,
-            Some(hex_proposal_block),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-        )))
-        .await?
-    else {
-        panic!(
-            "this getblocktemplate call should return the `ProposalMode` variant of the response"
-        )
-    };
-
-    assert!(
-        block_proposal_result.is_valid(),
-        "block proposal should succeed"
-    );
-
-    // Submit the same block
     let submit_block_response = rpc
         .submit_block(HexData(proposal_block.zcash_serialize_to_vec()?), None)
         .await?;
@@ -4045,7 +3877,7 @@ async fn invalidate_and_reconsider_block() -> Result<()> {
     let _init_guard = zebra_test::init();
     let net = Network::new_regtest(
         ConfiguredActivationHeights {
-            nu7: Some(100),
+            nu6_3: Some(100),
             ..Default::default()
         }
         .into(),
