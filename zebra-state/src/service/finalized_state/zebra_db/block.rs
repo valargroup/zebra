82df3b804a95a05dc4d4dc211b8d3f7aafb8641c
roman <roman@osmosis.team>
2026-06-19 16:40:31 -0300

    fix(state): remove duplicate finalized block import

--- zebra-state/src/service/finalized_state/zebra_db/block.rs
+++ zebra-state/src/service/finalized_state/zebra_db/block.rs
@@ -44,7 +44,6 @@ use crate::{
         disk_format::{
             block::TransactionLocation,
             transparent::{AddressBalanceLocationUpdates, OutputLocation},
-            IntoDisk,
         },
         zebra_db::{metrics::block_precommit_metrics, ZebraDb},
         FromDisk, IntoDisk, RawBytes, PRUNING_METADATA,
