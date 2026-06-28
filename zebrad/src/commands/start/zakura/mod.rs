a60d7f2d0a01389a0ece8f82a92538d76fa525bf
evan-forbes <evan.samuel.forbes@gmail.com>
2026-06-28 10:49:00 -0500

    feat(zebrad): wire Zakura block sync through start orchestration

--- zebrad/src/commands/start/zakura/mod.rs
+++ zebrad/src/commands/start/zakura/mod.rs
@@ -18,8 +18,7 @@ pub(crate) use block_sync_driver::{
     apply_block_sync_body, block_apply_class, block_sync_missing_body_window,
     block_sync_needed_blocks_from_state, coalesce_ready_needed_block_queries,
     coalesce_stale_needed_block_queries, commit_block_sync_body, query_block_sync_needed_blocks,
-    BlockApplyClass, ZAKURA_BLOCK_SYNC_CHECKPOINT_FRONTIER_REFRESH_INTERVAL,
-    ZAKURA_BLOCK_SYNC_MISSING_BODY_WINDOW,
+    ZebradBlockApplyExecutor, ZAKURA_BLOCK_SYNC_MISSING_BODY_WINDOW,
 };
 pub(crate) use frontier::{query_block_sync_frontiers, verified_block_tip_from_state};
 #[cfg(test)]
@@ -34,6 +33,8 @@ pub(crate) use header_sync_driver::{
     zakura_header_sync_driver_startup, ZakuraHeaderSyncDriverHandles,
 };
 pub(crate) use throughput_probe::{BlocksyncThroughputProbe, BlocksyncThroughputSummary};
+#[cfg(test)]
+pub(crate) use zebra_network::zakura::BlockApplyClass;
 
 pub(crate) const ZAKURA_BLOCK_SYNC_DRIVER_TIMEOUT: Duration = Duration::from_secs(30);
 
